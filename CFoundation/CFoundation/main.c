72b43be183d99f1d0db02b9d9a239881fd17eeda
lijingfu <lijingfu@zsxq.com>
2024-02-09 10:00:41 +0800

    Xcode 项目

---
+++ CFoundation/CFoundation/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CFoundation
+//
+//  Created by Jingfu Li on 2024/2/9.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
