c2f942a0af405e2fb90d67d80faf77d0072e9902
lijingfu <lijingfu@zsxq.com>
2024-02-19 21:39:42 +0800

    链表方法

--- CFoundation/CFoundation/src/list.c
+++ CFoundation/CFoundation/src/list.c
@@ -6,3 +6,27 @@
 //
 
 #include "list.h"
+
+int listAddValue(list_ref list, int value) {
+    return 0;
+}
+
+int listInsertValueAtIndex(list_ref list, int value, int index) {
+    return 0;
+}
+
+int listGetCount(list_ref list) {
+    return 0;
+}
+
+int listRemoveAtIndex(list_ref list, int index) {
+    return 0;
+}
+
+int listRemoveFirst(list_ref list) {
+    return 0;
+}
+
+int listRemoveLast(list_ref list) {
+    return 0;
+}
--- CFoundation/CFoundation/src/list.h
+++ CFoundation/CFoundation/src/list.h
@@ -10,14 +10,25 @@
 
 #include <stdio.h>
 
-struct list_node {
-};
-
 typedef struct list_node* list_node_ref;
+typedef struct list* list_ref;
+
+struct list_node {
+    int value;
+    list_node_ref nextNode;
+};
 
 struct list {
     int count;
     list_node_ref firstNode;
+    list_node_ref lastNode;
 };
+
+int listAddValue(list_ref list, int value);
+int listInsertValueAtIndex(list_ref list, int value, int index);
+int listGetCount(list_ref list);
+int listRemoveAtIndex(list_ref list, int index);
+int listRemoveFirst(list_ref list);
+int listRemoveLast(list_ref list);
 
 #endif /* list_h */
